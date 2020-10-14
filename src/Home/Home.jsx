48847efca80fa225c888f6f0fdc5d8411e312a95
Raghav Walia <raghavw@geekyants.com>
2020-10-14 18:37:52 +0530

    create chatroom using socket.io

---
+++ src/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
+
+const Home = () => {
+  const [roomName, setRoomName] = React.useState("");
+
+  const handleRoomNameChange = (event) => {
+    setRoomName(event.target.value);
+  };
+
+  return (
+    <div className="home-container">
+    <code className="text-input-field">Create or Join a room</code>
+      <input
+        type="text"
+        placeholder="Room"
+        value={roomName}
+        onChange={handleRoomNameChange}
+        className="text-input-field"
+      />
+      <Link to={`/${roomName}`} className="enter-room-button">
+        Join room
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
