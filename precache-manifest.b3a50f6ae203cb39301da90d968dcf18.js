4d36dd58187eaf3f39d84bea0e3706cb299dbdd0
Raghav Walia <raghavw@geekyants.com>
2020-10-30 20:07:39 +0530

    Updates

---
+++ precache-manifest.b3a50f6ae203cb39301da90d968dcf18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ff4d8fea40f5b7129371882ba4c618",
+    "url": "/chat-room/index.html"
+  },
+  {
+    "revision": "fab34b20229c8b435366",
+    "url": "/chat-room/static/css/main.7ee22c06.chunk.css"
+  },
+  {
+    "revision": "6e0bfbadc2e001619e21",
+    "url": "/chat-room/static/js/2.4b99c4f1.chunk.js"
+  },
+  {
+    "revision": "f3a69483bc670b2da5d044f5dce3c392",
+    "url": "/chat-room/static/js/2.4b99c4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab34b20229c8b435366",
+    "url": "/chat-room/static/js/main.f09361ab.chunk.js"
+  },
+  {
+    "revision": "558820108f43fbc62a1a",
+    "url": "/chat-room/static/js/runtime-main.03afa777.js"
+  }
+]);
