5bb9360b85c9b051597bded6c48b3100d6e2a17a
Raghav Walia <raghavw@geekyants.com>
2020-10-30 20:13:13 +0530

    Updates

---
+++ precache-manifest.55ebca5d0d045ec00fec2708777b70f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a62171b6d9ad83c53287f6773c4c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00dd242f0613612acf7",
+    "url": "/static/css/main.7ee22c06.chunk.css"
+  },
+  {
+    "revision": "445003034eb8d4923b3c",
+    "url": "/static/js/2.49da1fd1.chunk.js"
+  },
+  {
+    "revision": "f3a69483bc670b2da5d044f5dce3c392",
+    "url": "/static/js/2.49da1fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00dd242f0613612acf7",
+    "url": "/static/js/main.ebf955c8.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  }
+]);
